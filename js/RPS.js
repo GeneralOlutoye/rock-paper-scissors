c47c3e3b31dca374754ef37cac3f3eaadd12c040
GeneralOlutoye <donronald27@gmail.com>
2022-05-10 14:18:37 +0100

    Updated RPS

--- js/RPS.js
+++ js/RPS.js
@@ -1,11 +1,9 @@
-let PlayerName, display, computer, startBtn, player, player1, player2, 
-result,rock, papper, scissors;
+let PlayerName, display, computer, startBtn, player, player1, 
+result,choiceBtn;
 
 startBtn= document.getElementById('start')
 result= document.getElementById('result')
-rock= document.getElementById('rock')
-papper= document.getElementById('papper')
-scissors= document.getElementById('scissors')
+choiceBtn= document.querySelectorAll('choiceBtn')
 
 startBtn.addEventListener('click', ()=> {
     let name= PlayerName.value
@@ -13,9 +11,14 @@ startBtn.addEventListener('click', ()=> {
     player1.textContent= `${player}:`
 })
 
-rock.addEventListener('click', ()=> {
-    display= 'rock'
-    player1.textContent= `${player}: ${display}`
+choiceBtn.forEach(button => button.addEventListener('click', ()=> {
+    player= button.textContent;
+    computerTurn()
+    player1.textContent= `${player}: ${display}`  
     result.textContent= checkWinner()
     computerText.textContent= `computer : ${computer}`
-})
+}))
+
+computerTurn =>{
+    const randNum= Math.floor(Math.random()*3)+1
+}
