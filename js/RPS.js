96cbc4573f2d4774b816997cc82c4a2d45a4f1cc
GeneralOlutoye <donronald27@gmail.com>
2022-05-12 14:03:18 +0100

    Updated RPS

--- js/RPS.js
+++ js/RPS.js
@@ -1,24 +1,50 @@
-let PlayerName, display, computer, startBtn, player, player1, 
-result,choiceBtn;
+let computer, startBtn, player1, player, result,choiceBtn;
 
-startBtn= document.getElementById('start')
+// startBtn= document.getElementById('start')
+player1= document.querySelector('#player1')
 result= document.getElementById('result')
 choiceBtn= document.querySelectorAll('choiceBtn')
 
-startBtn.addEventListener('click', ()=> {
-    let name= PlayerName.value
-    player= name.toUpperCase()
-    player1.textContent= `${player}:`
-})
+// startBtn.addEventListener('click', ()=> {
+//     let name= PlayerName.value
+//     player= name.toUpperCase()
+//     player1.textContent= `${player}:`
+// })
 
 choiceBtn.forEach(button => button.addEventListener('click', ()=> {
     player= button.textContent;
     computerTurn()
-    player1.textContent= `${player}: ${display}`  
-    result.textContent= checkWinner()
+    player1.textContent= `Player: ${player}`  
     computerText.textContent= `computer : ${computer}`
+    result.textContent= checkWinner()
 }))
 
 computerTurn =>{
-    const randNum= Math.floor(Math.random()*3)+1
+    const randNum= Math.floor(Math.random()*3) +1
+
+    switch(randNum){
+        case 1:
+            computer= 'ROCK'
+            break;
+        case 2: 
+            computer= 'PAPER'
+            break;
+        case 3:
+            computer= 'SCISSORS'
+            break;
+    }
+}
+function checkWinner(){
+    if(player==computer){
+        return `DRAW`
+    }
+    else if(computer==`ROCK`){
+        return(player==`PAPER`) ? `YOU WIN` : `YOU LOSE`
+    }
+    else if(computer==`PAPER`){
+        return(player==`SCISSORS`)? `YOU WIN` : `YOU LOSE`
+    }
+    else if(computer==`SCISSORS`){
+        return(player==`ROCK`)? `YOU WIN` : `YOU LOSE`
+    }
 }
